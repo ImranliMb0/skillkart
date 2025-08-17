71c2ce96c9725959ba3b650749a62ae4807a47c5
Mohd Imran <121624508+ImranliMb0@users.noreply.github.com>
2025-08-17 17:28:40 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1h_qzTYJxVmH2DYa04z-F2o6gzvwvfEg",
-  authDomain: "skillkart-964bf.firebaseapp.com",
-  projectId: "skillkart-964bf",
-  storageBucket: "skillkart-964bf.appspot.com",
-  messagingSenderId: "146446808537",
-  appId: "1:146446808537:web:4a2460e895b90c1c11d5c8",
-  measurementId: "G-S7H727VY1N",
+  apiKey: "your api",
+  authDomain: "your project domain",
+  projectId: "project id",
+  storageBucket: "bucket",
+  messagingSenderId: "messagingsenderid",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
